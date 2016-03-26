090b69e727d8170d27c2a68b48009bc0e55a770c
Daniel Fireman <danielfireman@gmail.com>
2016-03-26 00:09:54 -0300

    Answering 2 questions

---
+++ fpcc2/a1m3/data_load.R
@@ -0,0 +1,5 @@
+data.01.01 <- read.csv("data/1jan.csv")
+data.2012.01.01 <- filter(data.01.01, Year == 2012)
+data.2013.01.01 <- filter(data.01.01, Year == 2013)
+data.2014.01.01 <- filter(data.01.01, Year == 2014)
+data.2015.01.01 <- filter(data.01.01, Year == 2015)
