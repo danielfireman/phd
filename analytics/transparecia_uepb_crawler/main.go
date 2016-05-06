ef852d0f958cc00fd3b782c81d61bc3f3d5fdbe3
Daniel Fireman <danielfireman@gmail.com>
2016-05-06 15:03:49 -0300

    Retries and other improvements

--- analytics/transparecia_uepb_crawler/main.go
+++ analytics/transparecia_uepb_crawler/main.go
@@ -5,8 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/jackdanger/collectlinks"
@@ -18,6 +21,8 @@ var mes = flag.String("mes", "01/2016", "mm/yyyy de referência.")
 var maxWorkers = flag.Int("max_workers", 20, "Número máximo de workers.")
 
 func main() {
+	startTime := time.Now()
+
 	flag.Parse()
 
 	if len(*mes) == 0 {
@@ -38,8 +43,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	links := make(chan string)
-	results := make(chan string)
+	links := make(chan string, *maxWorkers)
+	results := make(chan string, *maxWorkers)
 
 	for i := 0; i < *maxWorkers; i++ {
 		go worker(i, links, results)
@@ -57,28 +62,38 @@ func main() {
 	}
 
 	close(results)
+	fmt.Fprintf(os.Stderr, "\nFinished! Duration: %s", time.Now().Sub(startTime))
 }
+
+const maxRetries = 3
 
 func worker(id int, links <-chan string, results chan<- string) {
 	for link := range links {
-		resp, err := http.Get(link)
-		if err != nil {
-			log.Printf("Erro tentando buscar informações: %s. Erro: %q", link, err)
-			return
+		var doc *goquery.Document
+		for i := 1; ;i++ {
+			var err error
+			doc, err = goquery.NewDocument(link)
+			if err == nil {
+				break
+			}
+			fmt.Fprintf(os.Stderr, "[Tentativa %d] Erro tentando processar página de servidor: %s. Erro: %q", i, link, err)
+			if i == maxRetries {
+				fmt.Fprintf(os.Stderr, "Página não processada: %s", link)
+				return
+			}
+			time.Sleep(time.Duration(i) * time.Duration (rand.Intn(5)) * time.Second)
 		}
-		defer resp.Body.Close()
-		doc, err := goquery.NewDocument(link)
-		if err != nil {
-			log.Printf("Erro tentando processar página de servidor: %s. Erro: %q", link, err)
-			return
-		}
-		var rows []string
-		doc.Find("td").Each(func(i int, s *goquery.Selection) {
+		var row []string
+		doc.Find("td.desc").Each(func(i int, s *goquery.Selection) {
 			r := strings.Trim(s.Next().Text(), " \n")
 			if len(r) > 0 {
-				rows = append(rows, r)
+				row = append(row, r)
 			}
 		})
-		results <- strings.Join(rows, ";")
+		if len(row) > 0 {
+			results <- strings.Join(row, ";")
+		} else {
+			fmt.Fprintf(os.Stderr, "Não achou td.desc: %s\n", link)
+		}
 	}
 }
