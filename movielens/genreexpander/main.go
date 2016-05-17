a9b221ee14bb0d9c7770708f3b1a473c12f619e1
Daniel Fireman <danielfireman@gmail.com>
2016-05-17 16:32:24 -0300

    Fixes in main.go

--- movielens/genreexpander/main.go
+++ movielens/genreexpander/main.go
@@ -32,50 +32,54 @@ var genreMap = map[string]int{
 
 var yearRegexp = regexp.MustCompile(`\(\d+\)`)
 
+var csvReader = csv.NewReader
+
 func main() {
-	records, err := csv.NewReader(os.Stdin).ReadAll()
+	records, err := csvReader(os.Stdin).ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	for _, members := range records {
 		// Very simple way to skip the title.
 		if members[0] == "movieId" && members[1] == "title" && members[2] == "genres" {
 			continue
 		}
-		genres := strings.Split(members[2], "|")
-
-		// id, title, year, #genres
-		nFixedFields := 4
-		newRow := make([]string, nFixedFields+len(genreMap))
-		newRow[0] = members[0]
-
-		title := members[1]
-		// Making sure title is treated as string
-		newRow[1] = fmt.Sprintf("\"%s\"", title)
-
-		// Extracting year
-		newRow[2] = ""
-		matches := yearRegexp.FindAllString(title, -1)
-		if len(matches) > 0 {
-			withParenthesis := matches[len(matches)-1]
-			newRow[2] = withParenthesis[1 : len(withParenthesis)-1]
-		}
-
-		numGenres := 0
-		for _, g := range genres {
-			if g != "(no genres listed)" {
-				newRow[nFixedFields+genreMap[g]] = "1"
-				numGenres++
-			}
-		}
-		newRow[nFixedFields-1] = fmt.Sprintf("%d", numGenres)
-		// Filling out genres
-		for i, s := range newRow[nFixedFields:] {
-			if s == "" {
-				newRow[i+nFixedFields] = "0"
-			}
-		}
-		fmt.Println(strings.Join(newRow, ","))
+		fmt.Println(processRow(members))
 	}
+}
+
+func processRow(members []string) string {
+	// id, title, year, #genres
+	nFixedFields := 4
+	newRow := make([]string, nFixedFields+len(genreMap))
+	newRow[0] = members[0]
+
+	title := members[1]
+	// Making sure title is treated as string
+	newRow[1] = fmt.Sprintf("\"%s\"", title)
+
+	// Extracting year
+	newRow[2] = ""
+	matches := yearRegexp.FindAllString(title, -1)
+	if len(matches) > 0 {
+		withParenthesis := matches[len(matches)-1]
+		newRow[2] = withParenthesis[1 : len(withParenthesis)-1]
+	}
+
+	genres := strings.Split(members[2], "|")
+	numGenres := 0
+	for _, g := range genres {
+		if g != "(no genres listed)" {
+			newRow[nFixedFields+genreMap[g]] = "1"
+			numGenres++
+		}
+	}
+	newRow[nFixedFields-1] = fmt.Sprintf("%d", numGenres)
+	// Filling out genres
+	for i, s := range newRow[nFixedFields:] {
+		if s == "" {
+			newRow[i+nFixedFields] = "0"
+		}
+	}
+	return strings.Join(newRow, ",")
 }
