20096ad0f0d0f6cbaf59626fc0d9309bb3d08d60
Daniel Fireman <danielfireman@gmail.com>
2016-06-23 16:15:36 -0300

    Added logger

--- projfpcc/restserver/src/main/java/com/danielfireman/phd/App.java
+++ projfpcc/restserver/src/main/java/com/danielfireman/phd/App.java
@@ -6,7 +6,6 @@ import java.lang.management.OperatingSystemMXBean;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.jooby.Jooby;
 import org.jooby.Results;
@@ -17,27 +16,30 @@ import org.slf4j.LoggerFactory;
 public class App extends Jooby {
 	{
 		use(new Jackson());
-	
-		final AtomicInteger reqCount = new AtomicInteger(0);
-		final AtomicInteger reqCompleted = new AtomicInteger(0);
 
-		complete("/echo", (req, resp, cause) -> {
-			reqCompleted.incrementAndGet();
-		});
+		onStart(()->startLogger());
 
-		before("/echo", (req, resp) -> {
-			reqCount.incrementAndGet();
+		get("/msg", () -> {
+			Message msg = new Message();
+			msg.content = "My super big top ultra big content";
+			return Results.json(msg);
 		});
-		
-		post("/echo", (req, resp) -> {
-			resp.send(Results.json(req.body().to(Message.class)));
-		});
-				
-		final Logger logger = LoggerFactory.getLogger("mon");
+	}
+
+	private static void startLogger() {
+		final Logger gcLogger = LoggerFactory.getLogger("gc");
+		gcLogger.info("timestamp,name,colcount,coltime");
+
+		final Logger cpuLogger = LoggerFactory.getLogger("cpu");
+		cpuLogger.info("timestamp,systemload,nprocs");
+
 		final ScheduledExecutorService memSchedulerLogger = Executors.newScheduledThreadPool(1);
 		memSchedulerLogger.scheduleAtFixedRate(new Runnable() {
 			@Override
 			public void run() {
+				long time = System.currentTimeMillis();
+
+				// ## GC ##
 				for (GarbageCollectorMXBean bean : ManagementFactory.getGarbageCollectorMXBeans()) {
 					// colcount: total number of collections that have occurred.
 					// This method returns -1 if the collection count is
@@ -46,10 +48,11 @@ public class App extends Jooby {
 					// coltime: approximate accumulated collection elapsed time
 					// in milliseconds. This method returns -1 if the collection
 					// elapsed time is undefined for this collector.
-					logger.info(String.format("gc name:%s colcount:%s coltime:%s", bean.getName(),
-							bean.getCollectionCount(), bean.getCollectionTime()));
+					gcLogger.info(String.format("%s,\"%s\",%s,%s", time, bean.getName(), bean.getCollectionCount(),
+							bean.getCollectionTime()));
 				}
 
+				// ## CPU ##
 				// systemload: the system load average for the last minute. The
 				// system load average is the sum of the number of runnable
 				// entities queued to the available processors and the number of
@@ -60,13 +63,11 @@ public class App extends Jooby {
 				// Other reference:
 				// http://blog.scoutapp.com/articles/2009/07/31/understanding-load-averages
 				OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
-				logger.info(String.format("cpu systemload:%s nproc:%s", osBean.getSystemLoadAverage(), osBean.getAvailableProcessors()));
-
-				logger.info(String.format("req count:%s completed:%s", reqCount, reqCompleted));
+				cpuLogger.info(String.format("%s,%s,%s", time, osBean.getSystemLoadAverage(), osBean.getAvailableProcessors()));
 			}
-		}, 0, 30, TimeUnit.SECONDS);
+		}, 0, 10, TimeUnit.SECONDS);
 	}
-	
+
 	public static void main(final String[] args) throws Throwable {
 		run(App::new, args);
 	}
--- projfpcc/restserver/src/test/java/com/danielfireman/phd/AppTest.java
+++ projfpcc/restserver/src/test/java/com/danielfireman/phd/AppTest.java
@@ -6,8 +6,6 @@ public class AppTest extends BaseTest {
 
   @Test
   public void index() throws Exception {
-    server.post("/echo")
-    	.body("\"content\":\"Hi\"", "application/json;charset=UTF-8")
-    	.expect(200);
+    server.get("/msg").expect(200);
   }
 }
