e34699de225bfe431479e640494259343e01d820
Daniel Fireman <danielfireman@gmail.com>
2016-07-15 18:04:42 +0000

    variables and multiple runs in client run.sh

--- loadgen/run.sh
+++ loadgen/run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -x
 killall main
+rm logs/*
+
+WARMUP_STEPS=3
+ADDR=http://10.4.2.103:8080
+TIMEOUT=30ms
+STEP_DURATION=10s
+INITIAL_QPS=50
+STEP_SIZE=50
+MAX_QPS=1500
 for i in `seq 1 $1`
 do
-	sleep 30
-	go run main.go > logs/client_${i}_1 &
-	go run main.go > logs/client_${i}_2
-	sleep 30
+	sleep 1.5m
+	go run main.go --num_warmup_steps=${WARMUP_STEPS} --initial_qps=${INITIAL_QPS} --step_size=${STEP_SIZE} --timeout=${TIMEOUT} --max_qps=${MAX_QPS} --addr=${ADDR} > logs/client_${i}_1 &
+	go run main.go --num_warmup_steps=${WARMUP_STEPS} --initial_qps=${INITIAL_QPS} --step_size=${STEP_SIZE} --timeout=${TIMEOUT} --max_qps=${MAX_QPS} --addr=${ADDR} > logs/client_${i}_2
 done
