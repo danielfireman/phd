557089d503a5240bac8a73ac694fcda64e5c36ee
Daniel Fireman <danielfireman@gmail.com>
2016-07-19 23:30:40 -0300

    Making run.sh ready to be called via ssh

--- loadgen/run.sh
+++ loadgen/run.sh
@@ -4,15 +4,20 @@ killall main
 rm logs/*
 
 WARMUP_STEPS=3
-ADDR=http://10.4.2.103:8080
 TIMEOUT=30ms
 STEP_DURATION=10s
 INITIAL_QPS=50
 STEP_SIZE=50
-MAX_QPS=1500
+MAX_QPS=1000
 for i in `seq 1 $1`
 do
 	sleep 1.5m
-	go run main.go --num_warmup_steps=${WARMUP_STEPS} --initial_qps=${INITIAL_QPS} --step_size=${STEP_SIZE} --timeout=${TIMEOUT} --max_qps=${MAX_QPS} --addr=${ADDR} > logs/client_${i}_1 &
-	go run main.go --num_warmup_steps=${WARMUP_STEPS} --initial_qps=${INITIAL_QPS} --step_size=${STEP_SIZE} --timeout=${TIMEOUT} --max_qps=${MAX_QPS} --addr=${ADDR} > logs/client_${i}_2
+	id="`cat /var/lib/dbus/machine-id`"
+	./loadgen \
+--num_warmup_steps=${WARMUP_STEPS} \
+--initial_qps=${INITIAL_QPS} \
+--step_size=${STEP_SIZE} \
+--timeout=${TIMEOUT} \
+--max_qps=${MAX_QPS} \
+--addr=$2 > logs/client_${1}_{id}
 done
