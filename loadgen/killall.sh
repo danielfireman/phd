3e86ffb031f2dceaee2dbb369bd13d8539a34421
Daniel Fireman <danielfireman@gmail.com>
2016-07-20 10:58:44 -0300

    Moar fixes

--- loadgen/killall.sh
+++ loadgen/killall.sh
@@ -4,7 +4,7 @@ declare -a CLIENTS=('10.4.5.132' '10.4.5.130' '10.4.5.133' '10.4.5.134')
 echo "Time to kill bastard loadgen processes running on ${CLIENTS[@]}"
 for CLIENT in ${CLIENTS[@]};
 do
-	ssh -i ~/fireman.sururu.key ubuntu@${CLIENT} 'killall loadgen > /dev/null 2>/dev/null' &
+	ssh -i ~/fireman.sururu.key ubuntu@${CLIENT} 'killall loadgen > /dev/null 2>/dev/null'
 	echo "Loadgen process at ${CLIENT} ... killed."
 done
 echo "All killed. Please give me a harder task next time."
--- loadgen/run_multi.sh
+++ loadgen/run_multi.sh
@@ -2,10 +2,10 @@
 set -x
 
 declare -a CLIENTS=('10.4.5.132' '10.4.5.130' '10.4.5.133' '10.4.5.134')
-NUM_ROUNDS=$1
-SERVER=http://10.4.2.103:8080
+NUM_ROUNDS="$1"
+SERVER="http://10.4.2.103:8080"
 
 for CLIENT in ${CLIENTS[@]};
 do
-	ssh -i ~/fireman.sururu.key ubuntu@${CLIENT} 'cd ~/phd/loadgen;./run.sh ${NUM_ROUNDS} ${SERVER}' &
+	ssh -i ~/fireman.sururu.key ubuntu@${CLIENT} "cd ~/phd/loadgen;./run.sh ${NUM_ROUNDS} ${SERVER}" &
 done
