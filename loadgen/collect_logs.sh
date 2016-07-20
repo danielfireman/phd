a5385968bed45777a3e9d05ce4bfb100a8ff1737
Daniel Fireman <danielfireman@gmail.com>
2016-07-20 17:05:32 -0300

    Moar fixes

--- loadgen/collect_logs.sh
+++ loadgen/collect_logs.sh
@@ -14,7 +14,7 @@ ACTIVE_CLIENT=1
 for CLIENT in ${CLIENTS[@]};
 do
 	fname="c${NUM_CORES}_${EXECUTION_ID}_${ACTIVE_CLIENT}.zip"
-	ssh -i ~/fireman.sururu.key ubuntu@${CLIENT} "cd ~/phd/loadgen/logs; zip ${fname} *"
+	ssh -i ~/fireman.sururu.key ubuntu@${CLIENT} "cd ~/phd/loadgen/logs; zip ${fname} client*"
 	scp -i ~/fireman.sururu.key ubuntu@${CLIENT}:~/phd/loadgen/logs/${fname} ~/expresults_fpcc/
 	scp ~/expresults_fpcc/${fname} danielfireman@siri.lsd.ufcg.edu.br:/tmp/fireman_${fname}
 	if [ $ACTIVE_CLIENT == $NUM_CLIENTS ]; then
