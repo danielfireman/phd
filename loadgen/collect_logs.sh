51a5a253abaa2783d5f4119992b8ab1001db78f3
Daniel Fireman <danielfireman@gmail.com>
2016-07-20 16:50:24 -0300

    Fixing collect logs

--- loadgen/collect_logs.sh
+++ loadgen/collect_logs.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -x
 
+if [[ -z "$1" ]]; then
+	echo "Invalid execution id"
+	exit -1
+fi
+
 source configrc
 
 EXECUTION_ID=$1
@@ -11,7 +16,7 @@ do
 	fname="c${NUM_CORES}_${EXECUTION_ID}_${ACTIVE_CLIENT}.zip"
 	ssh -i ~/fireman.sururu.key ubuntu@${CLIENT} "cd ~/phd/loadgen/logs; zip ${fname} *"
 	scp -i ~/fireman.sururu.key ubuntu@${CLIENT}:~/phd/loadgen/logs/${fname} ~/expresults_fpcc/
-	scp ~/${fname} danielfireman@siri.lsd.ufcg.edu.br:/tmp/fireman_${fname}
+	scp ~/expresults_fpcc/${fname} danielfireman@siri.lsd.ufcg.edu.br:/tmp/fireman_${fname}
 	if [ $ACTIVE_CLIENT == $NUM_CLIENTS ]; then
 		break
 	fi
