43d5db03feaf798402887916093c2ccc6da60287
Daniel Fireman <danielfireman@gmail.com>
2016-09-11 14:17:45 -0300

    Changing collect logs to work with public LSD cloud ssh server

--- loadgen/collect_logs.sh
+++ loadgen/collect_logs.sh
@@ -14,10 +14,10 @@ ACTIVE_CLIENT=1
 echo "Packing client logs"
 for CLIENT in ${CLIENTS[@]};
 do
+	port=$(getport ${CLIENT})
 	fname="c${NUM_CORES}_${EXECUTION_ID}_${ACTIVE_CLIENT}.zip"
-	ssh -i ~/fireman.sururu.key ubuntu@${CLIENT} "cd ~/phd/loadgen/logs; zip ${fname} client*"
-	scp -i ~/fireman.sururu.key ubuntu@${CLIENT}:~/phd/loadgen/logs/${fname} ~/expresults_fpcc/
-	scp ~/expresults_fpcc/${fname} danielfireman@siri.lsd.ufcg.edu.br:/tmp/fireman_${fname}
+	ssh -i ~/fireman.sururu.key ${SSH_ADDR} -p ${port}  "cd ~/phd/loadgen/logs; zip ${fname} client*"
+	scp -P ${port} -i ~/fireman.sururu.key ${SSH_ADDR}:~/phd/loadgen/logs/${fname} /tmp/explogs_${fname}
 	if [ $ACTIVE_CLIENT == $NUM_CLIENTS ]; then
 		break
 	fi
@@ -27,6 +27,6 @@ done
 echo "Packing server logs"
 sfname="s${NUM_CORES}_${EXECUTION_ID}.zip"
 spath="~/phd/projfpcc/restserver/logs"
-ssh -i ~/fireman.sururu.key ubuntu@${SERVER_IP} "cd ${spath}; zip ${sfname} *.csv"
-scp -i ~/fireman.sururu.key ubuntu@${SERVER_IP}:${spath}/${sfname} ~/expresults_fpcc/
-scp ~/expresults_fpcc/${sfname} danielfireman@siri.lsd.ufcg.edu.br:/tmp/fireman_${sfname}
+sport=$(getport ${SERVER_IP})
+ssh -i ~/fireman.sururu.key ${SSH_ADDR} -p ${sport} "cd ${spath}; zip ${sfname} *.csv"
+scp -P ${sport} -i ~/fireman.sururu.key ${SSH_ADDR}:${spath}/${sfname} /tmp/explogs_${sfname}
