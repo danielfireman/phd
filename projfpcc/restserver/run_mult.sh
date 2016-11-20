8b816380be18518d5c2230dfe01ce2563338076f
Daniel Fireman <danielfireman@gmail.com>
2016-11-20 14:53:10 -0300

    FIxing run_multi

--- projfpcc/restserver/run_mult.sh
+++ projfpcc/restserver/run_mult.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -x
 rm logs/*
+mvn clean package -DskipTests
 for i in `seq 1 $1`
 do
-	export ROUND=$i; export CPU_LOG_FILE=cpu_$i; mvn jooby:run
+	export ROUND=$i; export CPU_LOG_FILE=cpu_$i; java  -Xmx1024m -Xms1024m -XX:NewRatio=1 -XX:InitialCodeCacheSize=32m -XX:MetaspaceSize=32m -jar target/restserver-1.0.jar
 	killall java
 	sleep 10s
 done
