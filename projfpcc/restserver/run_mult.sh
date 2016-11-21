9d79be3b5a54e1795e997220e12f005c56ccbaf4
Daniel Fireman <danielfireman@gmail.com>
2016-11-21 11:27:25 -0300

    Fixing flags

--- projfpcc/restserver/run_mult.sh
+++ projfpcc/restserver/run_mult.sh
@@ -4,7 +4,12 @@ rm logs/*
 mvn clean package -DskipTests
 for i in `seq 1 $1`
 do
-	export ROUND=$i; export CPU_LOG_FILE=cpu_$i; java  -Xmx1024m -Xms1024m -XX:NewRatio=1 -XX:InitialCodeCacheSize=32m -XX:MetaspaceSize=32m -jar target/restserver-1.0.jar
+	VM_FLAGS=""
+	export ROUND=$i; export CPU_LOG_FILE=cpu_$i; 
+	if [ -n "${CONTROL_GC}" ]; then
+		VM_FLAGS="-Xmx1024m -Xms1024m -XX:NewRatio=1 -XX:InitialCodeCacheSize=32m -XX:MetaspaceSize=32m"
+	fi
+	java ${VM_FLAGS} -jar target/restserver-1.0.jar
 	killall java
 	sleep 10s
 done
